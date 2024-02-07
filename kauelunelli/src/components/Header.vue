e1ab2d1eb3d78b35e26fd7b05cda61fb6a5baf15
kauelunelli <lunellikaue@gmail.com>
2024-02-07 18:06:02 -0300

    feat: preparacao pagina inicial

---
+++ kauelunelli/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IconLinkedl from './icons/IconLinkedl.vue'
+</script>
+<template>
+  <div>
+    <div class="header-container">
+      <h1>Kaue o real Dets</h1>
+      <icon-linkedl />
+    </div>
+  </div>
+</template>
+<style scoped>
+.header-container {
+  background: rgb(188, 181, 181);
+  width: 100%;
+  max-height: 100px;
+  margin: auto;
+}
+</style>
---
+++ kauelunelli/src/components/SideNavigation.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const teste = ref('Teste taligado')
+</script>
+<template>
+  <main>
+    <p>{{ teste }}</p>
+  </main>
+</template>
