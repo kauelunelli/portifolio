e778aa2854e0d3ebb69e43555cd49209be48e88b
kauelunelli <lunellikaue@gmail.com>
2024-02-07 18:05:15 -0300

    feat: criacao das regras

--- kauelunelli/.eslintrc.cjs
+++ kauelunelli/.eslintrc.cjs
@@ -3,12 +3,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  },
   overrides: [
     {
       files: [
@@ -16,9 +19,7 @@ module.exports = {
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
         'cypress/support/**/*.{js,ts,jsx,tsx}'
       ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
--- kauelunelli/env.d.ts
+++ kauelunelli/env.d.ts
@@ -1,1 +1,5 @@
-/// <reference types="vite/client" />
+declare module '*.svg' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
