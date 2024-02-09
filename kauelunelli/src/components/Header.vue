76148798e9e0e671890d74a7ac8994388638d289
kauelunelli <lunellikaue@gmail.com>
2024-02-08 22:02:38 -0300

    feat: criacao da navbar

--- kauelunelli/src/components/Header.vue
+++ kauelunelli/src/components/Header.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-import IconLinkedl from './icons/IconLinkedl.vue'
+import { IconDarkMode, IconLinkedl } from './icons/index'
 </script>
 <template>
   <div>
     <div class="header-container">
-      <h1>Kaue o real Dets</h1>
-      <icon-linkedl />
+      <div class="header-icon-container">
+        <IconLinkedl class="icon" />
+        <IconDarkMode class="icon" />
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
 .header-container {
-  background: rgb(188, 181, 181);
+  background: rgb(241, 236, 236);
   width: 100%;
   max-height: 100px;
-  margin: auto;
+  padding: 0 1.5rem;
+}
+.header-icon-container {
+  display: flex;
+  justify-content: right;
+}
+
+.icon {
+  margin: 12px 24px;
 }
 </style>
--- kauelunelli/src/views/HomeView.vue
+++ kauelunelli/src/views/HomeView.vue
@@ -5,6 +5,27 @@ import Header from '../components/Header.vue'
 <template>
   <main>
     <Header />
-    <p>Im nigga kosh in alchool</p>
+    <div class="apresentation-container">
+      <div class="apresentatition-title">
+        <h1>Kauê Lunelli</h1>
+      </div>
+      <div class="apresentatition-title">
+        <h2>Web Developer</h2>
+      </div>
+      <div class="apresentatition-title">
+        <h1>Texto</h1>
+      </div>
+    </div>
   </main>
 </template>
+<style scoped>
+.apresentation-container {
+  display: flex;
+  justify-content: center;
+  height: 800px;
+  max-width: 80%;
+  margin: auto;
+}
+.apresentatition-title {
+}
+</style>
